2565302e12b1d9374f61075198b71d1f7e271b9b
Mike Carter <MCarter@ITSRND.COM>
2019-07-16 12:58:10 -0700

    changes

--- dialogs/mainDialog.js
+++ dialogs/mainDialog.js
@@ -11,12 +11,16 @@ const { DialogHelper } = require('./dialogHelper');
 const { LuisDialog } = require('./luisDialog');
 
 
+
+
 const MAIN_WATERFALL_DIALOG = 'mainWaterfallDialog';
 const SEARCH_FILTER_DIALOG = 'searchFilterDialog';
 const SEARCH_DIALOG = 'searchDialog';
 const CHOICE_PROMPT = 'CHOICE_PROMPT';
 
 const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
 const WelcomeCard = require('../bots/resources/welcomeCard.json');
 
 class MainDialog extends ComponentDialog {
@@ -127,11 +131,24 @@ class MainDialog extends ComponentDialog {
            var reply2 = MessageFactory.suggestedActions(['luis: Member account reports that are confidential','luis: Reports owned by Anthony', 'luis: Sensitive reports', 'LUIS Search Approved reports by John']);
            await turnContext.sendActivity(reply2);
 
-         }else if(turnContext.activity.text.includes('LUIS Search') || turnContext.activity.text.includes('luis search') || turnContext.activity.text.includes('luis:')){
+         }else if(turnContext.activity.text === 'LUIS Search' || turnContext.activity.text === 'luis search' || turnContext.activity.text === 'luis:'){
 
            await this.luisDialog.onTurn(turnContext);
 
          }
+
+         if (turnContext.activity.attachments && turnContext.activity.attachments.length > 0) {
+              // The user sent an attachment and the bot should handle the incoming attachment.
+              //await this.handleIncomingAttachment(turnContext);
+              await turnContext.sendActivity({ attachments: [this.dialogHelper.createBotCard('...Looks like you sent an attachment','I dont understand this quite yet')] });
+
+              await turnContext.sendActivity({ attachments: [this.dialogHelper.createBotCard('...Is there anything else I can help you with?','')] });
+
+              var reply = MessageFactory.suggestedActions(['How Do I Calculate the 2% Retirement Formula','Select a Report by Report Name', 'Search Options', 'Search with LUIS']);
+              await turnContext.sendActivity(reply);
+
+
+          }
 
      }
 
@@ -176,6 +193,127 @@ class MainDialog extends ComponentDialog {
       return await stepContext.endDialog();
 
     }
+
+    /**
+         * Saves incoming attachments to disk by calling `this.downloadAttachmentAndWrite()` and
+         * responds to the user with information about the saved attachment or an error.
+         * @param {Object} turnContext
+         */
+        async handleIncomingAttachment(turnContext) {
+            // Prepare Promises to download each attachment and then execute each Promise.
+            const promises = turnContext.activity.attachments.map(this.downloadAttachmentAndWrite);
+            const successfulSaves = await Promise.all(promises);
+
+            // Replies back to the user with information about where the attachment is stored on the bot's server,
+            // and what the name of the saved file is.
+            async function replyForReceivedAttachments(localAttachmentData) {
+                if (localAttachmentData) {
+                    // Because the TurnContext was bound to this function, the bot can call
+                    // `TurnContext.sendActivity` via `this.sendActivity`;
+                    await this.sendActivity(`Attachment "${ localAttachmentData.fileName }" ` +
+                        `has been received and saved to "${ localAttachmentData.localPath }".`);
+                } else {
+                    await this.sendActivity('Attachment was not successfully saved to disk.');
+                }
+            }
+
+            // Prepare Promises to reply to the user with information about saved attachments.
+            // The current TurnContext is bound so `replyForReceivedAttachments` can also send replies.
+            const replyPromises = successfulSaves.map(replyForReceivedAttachments.bind(turnContext));
+            await Promise.all(replyPromises);
+        }
+
+        /**
+         * Downloads attachment to the disk.
+         * @param {Object} attachment
+         */
+        async downloadAttachmentAndWrite(attachment) {
+            // Retrieve the attachment via the attachment's contentUrl.
+            const url = attachment.contentUrl;
+
+            // Local file path for the bot to save the attachment.
+            const localFileName = path.join('./bots/uploads', attachment.name);
+
+            try {
+                // arraybuffer is necessary for images
+                const response = await axios.get(url, { responseType: 'arraybuffer' });
+                // If user uploads JSON file, this prevents it from being written as "{"type":"Buffer","data":[123,13,10,32,32,34,108..."
+                if (response.headers['content-type'] === 'application/json') {
+                    response.data = JSON.parse(response.data, (key, value) => {
+                        return value && value.type === 'Buffer' ? Buffer.from(value.data) : value;
+                    });
+                }
+                fs.writeFile(localFileName, response.data, (fsError) => {
+                    if (fsError) {
+                        throw fsError;
+                    }
+                });
+            } catch (error) {
+                console.error(error);
+                return undefined;
+            }
+            // If no error was thrown while writing to disk, return the attachment's name
+            // and localFilePath for the response back to the user.
+            return {
+                fileName: attachment.name,
+                localPath: localFileName
+            };
+        }
+
+        /**
+         * Responds to user with either an attachment or a default message indicating
+         * an unexpected input was received.
+         * @param {Object} turnContext
+         */
+        async handleOutgoingAttachment(turnContext) {
+            const reply = { type: ActivityTypes.Message };
+
+            // Look at the user input, and figure out what type of attachment to send.
+            // If the input matches one of the available choices, populate reply with
+            // the available attachments.
+            // If the choice does not match with a valid choice, inform the user of
+            // possible options.
+            const firstChar = turnContext.activity.text[0];
+            if (firstChar === '1') {
+                reply.text = 'This is an inline attachment.';
+                reply.attachments = [this.getInlineAttachment()];
+            } else if (firstChar === '2') {
+                reply.attachments = [this.getInternetAttachment()];
+                reply.text = 'This is an internet attachment.';
+            } else if (firstChar === '3') {
+                reply.attachments = [await this.getUploadedAttachment(turnContext)];
+                reply.text = 'This is an uploaded attachment.';
+            } else {
+                // The user did not enter input that this bot was built to handle.
+                reply.text = 'Your input was not recognized, please try again.';
+            }
+            await turnContext.sendActivity(reply);
+        }
+
+
+        /**
+         * Returns an attachment that has been uploaded to the channel's blob storage.
+         * @param {Object} turnContext
+         */
+        async getUploadedAttachment(turnContext) {
+            const imageData = fs.readFileSync(path.join(__dirname, '../resources/architecture-resize.png'));
+            const connector = turnContext.adapter.createConnectorClient(turnContext.activity.serviceUrl);
+            const conversationId = turnContext.activity.conversation.id;
+            const response = await connector.conversations.uploadAttachment(conversationId, {
+                name: 'architecture-resize.png',
+                originalBase64: imageData,
+                type: 'image/png'
+            });
+
+            // Retrieve baseUri from ConnectorClient for... something.
+            const baseUri = connector.baseUri;
+            const attachmentUri = baseUri + (baseUri.endsWith('/') ? '' : '/') + `v3/attachments/${ encodeURI(response.id) }/views/original`;
+            return {
+                name: 'architecture-resize.png',
+                contentType: 'image/png',
+                contentUrl: attachmentUri
+            };
+        }
 
 }
 
